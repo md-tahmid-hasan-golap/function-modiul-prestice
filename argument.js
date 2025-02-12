cff69adbe561b3a6cd33f18b021385e149838136
golap-raj <golapraj47@gmail.com>
2025-02-12 12:11:01 -0600

    add

---
+++ argument.js
@@ -0,0 +1,29 @@
+// function add(num1, num2){
+//     const sum = num1 + num2;
+//     const biyog = num1 - num2;
+//     const borgo = sum * borgo;
+//     const total = biyog / total;
+//     console.log(total)
+
+
+// }
+// add(12, 66)
+
+function double (number){
+    const douled = number * 2;
+    console.log(douled)
+}
+double(100);
+double(55)
+
+
+function difference (num1, num2){
+    const diff = num1 - num2;
+    console.log(diff);
+
+}
+
+const father = 55;
+const my = 23
+difference(12, 4)
+difference(father, my)
