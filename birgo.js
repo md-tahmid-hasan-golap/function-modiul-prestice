d281f4b4ca2ecb15d1c37b4ce4d09f665e9da4a1
golap-raj <golapraj47@gmail.com>
2025-02-12 12:00:50 -0600

    add sum

---
+++ birgo.js
@@ -0,0 +1,30 @@
+// function square (number){
+//     console.log("my number ", number);
+//     const borgo = number * number;
+//     console.log(borgo)
+
+// }
+// square(10);
+// square(100);
+// square(44);
+// square(111)
+
+// function sum (a, b){
+//     console.log("number ", a, b)
+//         const add = a + b ;
+//         console.log(add)
+    
+
+// }
+// sum(23, 44,);
+// sum(11, 88);
+// sum(77, 44)
+
+
+function addAll(a, b, c, d, e){
+    console.log("all number sum ", a, b, c, d, e);
+    const total = a + b + c + d + e;
+    console.log(total)
+}
+addAll(12, 44, 77, 44, 90);
+addAll(55, 66, 12, 77, 45)
---
+++ function.js
@@ -0,0 +1,9 @@
+// function diclaration
+
+function persone(){
+    console.log("my name is golap");
+    console.log("my father name is a abdullah");
+    console.log("my mother name is a chemely");
+    console.log("my sister name is a taposhi")
+}
+persone();
