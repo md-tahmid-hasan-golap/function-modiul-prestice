23bcf4708e1133ae2c65540eabc383464505e25d
golap-raj <golapraj47@gmail.com>
2025-02-12 12:15:47 -0600

    return

---
+++ return.js
@@ -0,0 +1,13 @@
+function price(a, b){
+    const result = a + b;
+    return result
+
+}
+
+const output = price(12, 55)
+console.log(output);
+
+const myage = 20;
+const fatherage = 48;
+const mixage = price(myage, fatherage);
+console.log(mixage)
